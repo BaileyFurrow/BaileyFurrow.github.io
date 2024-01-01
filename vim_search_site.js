d286309617bd639da814d28b2af58a3def835167
Bailey Furrow <baileyfurrow24@gmail.com>
2024-01-01 11:51:34 -0500

    optimization and scrollIntoView()

--- vim_search_site.js
+++ vim_search_site.js
@@ -1,22 +1,25 @@
+/** Captures the slash (/) key when NOT in a text box and focuses input on
+ *  website's search field. In other words, activates search vim-style. */
 function vimSiteSearch(searchID) {
-  console.log('testing');
-  const SEARCH = document.querySelector(searchID);
+    const SEARCH = Node.prototype.isPrototypeOf(searchID)
+        ? searchID
+        : document.querySelector(searchID);
 
-  document.addEventListener('keyup', captureSlash);
-  SEARCH.addEventListener('focus', () => {
-      document.removeEventListener('keyup', captureSlash);
-  });
+    document.addEventListener("keyup", captureSlash);
+    SEARCH.addEventListener("focus", () => {
+        document.removeEventListener("keyup", captureSlash);
+    });
 
-  SEARCH.addEventListener('blur', () => {
-      document.addEventListener('keyup', captureSlash);
-  });
+    SEARCH.addEventListener("blur", () => {
+        document.addEventListener("keyup", captureSlash);
+    });
 
-  function captureSlash(event) {
-      console.log('key press');
-      console.log(event.key == '/' && document.activeElement.tagName != 'INPUT');
-      if (event.key == '/' && document.activeElement.tagName != 'INPUT') {
-          SEARCH.select();
-          SEARCH.focus();
-      }
-  }
+    function captureSlash(event) {
+        console.log("key press");
+        if (event.key == "/" && document.activeElement.tagName != "INPUT") {
+            SEARCH.select();
+            SEARCH.focus();
+            SEARCH.scrollIntoView();
+        }
+    }
 }
