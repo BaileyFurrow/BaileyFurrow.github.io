d3a457e28819d9dfc3df7737c2b882c881141ab9
Bailey Furrow <baileyfurrow24@gmail.com>
2024-01-24 21:16:11 -0500

    make vim search work with slower websites

--- vim_search_site.js
+++ vim_search_site.js
@@ -1,9 +1,24 @@
 /** Captures the slash (/) key when NOT in a text box and focuses input on
  *  website's search field. In other words, activates search vim-style. */
 function vimSiteSearch(searchID) {
-    const SEARCH = Node.prototype.isPrototypeOf(searchID)
-        ? searchID
-        : document.querySelector(searchID);
+    function waitForElem(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+            observer.observe(document.body, {
+                chistList: true,
+                subtree: true
+            });
+        });
+    }
+    const SEARCH = await waitForElem(searchID);
 
     document.addEventListener("keyup", captureSlash);
     SEARCH.addEventListener("focus", () => {
